175ed82a9d858049ce7ce52731649ff50d185ff6
jrkristo <jrkristo@mtu.edu>
2017-02-21 01:54:44 -0500

    dos

---
+++ prompt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+#include <editline/history.h>
+
+int main(int argc, char** argv)
+{
+	puts ("clispy ver -1");
+	puts ("Ctrl+c to exit");
+	while (1) 
+ 	{
+		/*prompt*/
+		
+		/*input*/
+		/*echo!*/
+		printf("echoing %s", input);
+	}
+}
